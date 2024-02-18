1a92c5bcf9ab14d2648830cb829221d31829cdff
Gyorgy Kernya <kernyag@gmail.com>
2024-02-18 14:02:48 +0100

    Reading daily data from stock API

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import requests
 import os
-from dotenv import load_env
+from dotenv import load_dotenv
+import json
 
-STOCK = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+STOCK = "AAPL"
+COMPANY_NAME = "Apple Inc"
+
+load_dotenv()
+STOCK_KEY = os.getenv("STOCK_API_KEY")
+
+stock_parameters ={
+    "function": "TIME_SERIES_DAILY",
+    "symbol": "TSLA",
+    "apikey": STOCK_KEY
+}
+
+stock_api_endpoint ="https://www.alphavantage.co/query"
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+response = requests.get(stock_api_endpoint, params=stock_parameters)
+response.raise_for_status()
+stock_data = response.json()
+
+"""with open("daily_AAPL.json", mode="w") as file:
+    json.dump(stock_data, file, indent=4)"""
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
